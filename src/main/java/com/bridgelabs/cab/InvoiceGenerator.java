275b888f087e2519192860493beb31199d0c29c2
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-29 16:52:40 +0530

    Added Test case to ensure that getting a minimum fare

--- src/main/java/com/bridgelabs/cab/InvoiceGenerator.java
+++ src/main/java/com/bridgelabs/cab/InvoiceGenerator.java
@@ -4,8 +4,11 @@ public class InvoiceGenerator {
     private final int COST_PER_KM = 10;
     private final int COST_PER_MINUTE = 1;
     private final int MIN_FARE = 5;
-    public double getFare(int distance, int time) {
+    public double getFare(double distance, int time) {
         double fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+        if(fare < MIN_FARE){
+            return  MIN_FARE;
+        }
         return fare;
     }
 }
--- src/test/java/com/bridgelabs/cab/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabs/cab/CabInvoiceGeneratorTest.java
@@ -10,4 +10,12 @@ public class CabInvoiceGeneratorTest {
         double fare = invoiceGenerator.getFare(4,20);
         Assert.assertEquals(60,fare,0.0);
     }
+
+    @Test
+    public void given_DistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fare = invoiceGenerator.getFare(0.200,1);
+        Assert.assertEquals(5,fare,0.0);
+
+    }
 }
